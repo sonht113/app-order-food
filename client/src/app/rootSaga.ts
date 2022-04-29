d528a1f72ccd168424db4851937f49cd7b95adce
sonht113 <trongson18101999@gmail.com>
2022-04-29 21:34:52 +0700

    [hotrongson] add redux-saga

---
+++ client/src/app/rootSaga.ts
@@ -0,0 +1,11 @@
+import {all} from 'redux-saga/effects'
+import counterSaga from '../features/counter/counterSaga';
+
+function* helloSaga() {
+  console.log('Hello saga');
+}
+
+export default function* rootSaga() {
+  console.log('Root Saga');
+  yield all([helloSaga(), counterSaga()])
+}
---
+++ client/src/features/counter/counterSaga.ts
@@ -0,0 +1,13 @@
+import {takeEvery} from 'redux-saga/effects'
+import { PayloadAction } from '@reduxjs/toolkit';
+
+import { increment } from './counterSlice'
+
+function* Log(action: PayloadAction) {
+  console.log("Log", action);
+}
+
+export default function* counterSaga() {
+  console.log('Counter saga');
+  yield takeEvery(increment().type, Log)
+}

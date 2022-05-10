728b427c3321b7eced81d685cfef385b6cd90ef0
sonht113 <trongson18101999@gmail.com>
2022-05-11 01:45:28 +0700

    create model interface

---
+++ client/src/models/Product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+  count: number;
+}
---
+++ client/src/models/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string;
+  username: string;
+  address: string;
+  phone: string;
+  avatar: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+}

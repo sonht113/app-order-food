4eaa11dbe3c039daa88a939d68ca74ceb7682aaa
sonht113 <trongson18101999@gmail.com>
2022-05-15 09:57:34 +0700

    [hotrongson] update get list product

--- client/src/components/Main/effects/ListProduct.tsx
+++ client/src/components/Main/effects/ListProduct.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderProductList from './HeaderProductList';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import productApi from '../../../api/productApi';
@@ -13,25 +13,22 @@ const ListProduct:React.FC = () => {
     productApi
       .getAll()
       .then((res) => {
-        //console.log(res);
         dispatch(productSlice.actions.setProducts(res))
       })
-      .catch((err: Error) => console.log("Error: ", err))
   }, [])
 
-  const result = useAppSelector(selectRemainingProductResult)
-  const listProduct:Product[] = result.products
-  //console.log(listProduct);
+  const listProduct: Product[] = useAppSelector(selectRemainingProductResult)
+  console.log(listProduct);
 
   return (
     <div className="Products">
       <HeaderProductList />
       <div className="List-Products grid md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-16 gap-y-16 md:gap-x-5 px-10 mt-14">
-        {   listProduct.length === 0
+        {   listProduct?.length === 0
           ?   <div className="w-2/3 mx-auto col-span-4 2xl:col-span-5">
             <h1 className="text-center text-2xl font-bold text-pink-600">Not found product :((( </h1>
           </div>
-          : listProduct.map((product: Product, index: number) => (
+          : listProduct?.map((product: Product, index: number) => (
             <div
               key={index}
               className="flex relative group flex-col items-center shadow-lg cursor-pointer py-5 rounded-lg transition group ease-in-out delay-150 hover:scale-110 duration-300"
